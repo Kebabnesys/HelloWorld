ebe63b02ee2f1b4fe9771c207175b594f263bd31
Elze <elzeposk56@gmail.com>
2026-09-16 15:09:00 +0300

    First Button

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.lab1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,10 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void onButtonClick(View view) {
+        TextView text = (TextView) findViewById(R.id.textView);
+        text.setText ("Button clicked");
     }
 }
